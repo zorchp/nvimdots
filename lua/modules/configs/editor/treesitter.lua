6be18f844715a2733308817221af99a459a14501
zorchp <zorchp@gmail.com>
2023-11-25 13:21:47 +0800

    change-vimtex-ts-conflict

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -9,14 +9,14 @@ return vim.schedule_wrap(function()
 		highlight = {
 			enable = true,
 			disable = function(ft, bufnr)
-				if vim.tbl_contains({ "vim" }, ft) then
+				if vim.tbl_contains({ "vim", "latex" }, ft) then
 					return true
 				end
 
 				local ok, is_large_file = pcall(vim.api.nvim_buf_get_var, bufnr, "bigfile_disable_treesitter")
 				return ok and is_large_file
 			end,
-			additional_vim_regex_highlighting = false,
+			additional_vim_regex_highlighting = { "latex", "markdown" },
 		},
 		textobjects = {
 			select = {
